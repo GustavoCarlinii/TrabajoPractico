5ee94f02508ee084b020cddbe86d27cc2329e709
GustavoCarlini <gcarlini52@gmail.com>
2025-06-12 09:25:29 -0300

    Creacion Proyecto

---
+++ Models/Direccion.cs
@@ -0,0 +1,17 @@
+﻿namespace CrudMVCApp.Models
+{
+    public class Direccion
+    {
+        public int Id { get; set; }
+        public string Calle { get; set; }
+        public string Ciudad { get; set; }
+        public string CodigoPostal { get; set; }
+        // Clave Foranea
+        public int PersonaId { get; set; }
+
+        // propiedad de navegación
+        public Persona Persona { get; set; }
+
+        public Direccion() { }
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMVCApp.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; } // Clave primaria necesaria para EF
+        public string Nombre { get; set; }
+       
+        [Display(Name = "Apellido del Cliente")]
+        public string Apellido { get; set; }
+        
+        public int Dni { get; set; }
+        public string Cuit { get; set; }
+        public bool Futbol { get; set; }
+        public bool Basquet { get; set; }
+        public bool Otros { get; set; }
+        public char Genero { get; set; }
+
+        //Propiedad navegacion 
+        public ICollection<Direccion> Direcciones { get; set; }
+        public Persona (){}
+        
+    }
+}
+
